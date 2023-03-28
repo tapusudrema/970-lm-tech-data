69527147d21e792efdce13c49227283d858da0f1
LEBD <muerebillgates@hotmail.com>
2023-03-28 13:33:11 -0500

    Merge remote-tracking branch 'origin/main'

--- projetos/calculadora/app.py
+++ projetos/calculadora/app.py
@@ -1,7 +1,4 @@
 # Importando a função do arquivo calculadora/__init__.py
-import os
-import sys
-sys.path.insert(0, os.getcwd())
 from projetos.calculadora import calcule
-# Executable a aplicação
+# Executar a aplicação
 calcule()
