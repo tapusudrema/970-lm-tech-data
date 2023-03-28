ea5fb8f1f2b3cf7c92945956c10bffb796f8550e
LEBD <muerebillgates@hotmail.com>
2023-03-28 13:24:28 -0500

    Merge remote-tracking branch 'origin/main'

--- projetos/calculadora/__init__.py
+++ projetos/calculadora/__init__.py
@@ -1,5 +1,15 @@
 #init
 from funcoes import tipos_certos, soma, subtracao, multiplicacao, divisao
+dictFuncoes = {
+    'soma': soma,
+    'subtracao': subtracao,
+    'divisao': divisao,
+    'multiplicacao': multiplicacao,
+    '+': soma,
+    '-': subtracao,
+    '/': divisao,
+    '*': multiplicacao
+}
 def calcule():
     a = input('primeiro número: ')
     b = input('segundo número: ')
@@ -10,14 +20,24 @@ def calcule():
         validacao = tipos_certos(a, b)
 
     if validacao:
-        operacao = ''
-        operacoes1 = ['soma', 'subtracao', 'divisao', 'multiplicacao']
-        operacoes2 = ['+', '-', '/', '*']
-        while operacao not in operacoes1 + operacoes2:
-            operacao = input('Qual operação? Opções válidas: soma, subtracao, divisao, multiplicacao, +, -, /, * ').strip().lower()
-        if operacao in operacoes2:
-            operacao = operacoes1[operacoes2.index(operacao)]
-        print(eval(operacao)(a, b))
-        print('fim.')
+        metodo1(a,b)
+        metodo2(a,b,dictFuncoes)
 
+def metodo1(a, b):
+    operacao = ''
+    operacoes1 = ['soma', 'subtracao', 'divisao', 'multiplicacao']
+    operacoes2 = ['+', '-', '/', '*']
+    while operacao not in operacoes1 + operacoes2:
+        operacao = input(
+            'Qual operação? Opções válidas: soma, subtracao, divisao, multiplicacao, +, -, /, * ').strip().lower()
+    if operacao in operacoes2:
+        operacao = operacoes1[operacoes2.index(operacao)]
+    print(eval(operacao)(a, b))
+    print('fim.')
 
+def metodo2(a, b, dictio):
+    operacao = ''
+    while operacao not in dictio.keys():
+        operacao = input(
+            'Qual operação? Opções válidas: soma, subtracao, divisao, multiplicacao, +, -, /, * ').strip().lower()
+    print(dictio)
