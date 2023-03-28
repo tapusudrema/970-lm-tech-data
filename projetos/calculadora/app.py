864291c821ee8a5900183eb66c323333343a9b88
LEBD <muerebillgates@hotmail.com>
2023-03-27 21:25:27 -0500

    Merge remote-tracking branch 'origin/main'

--- projetos/calculadora/app.py
+++ projetos/calculadora/app.py
@@ -1,2 +1,7 @@
 
 #listo
+# Importanto a função do arquivo calculadora/__init__.py
+from calculadora import calcule
+
+# Executando a aplicação
+calcule()
--- projetos/calculadora/calculadora/__init__.py
+++ projetos/calculadora/calculadora/__init__.py
@@ -1,1 +1,15 @@
 #init
+def calcule():
+    a = input('primeiro número: ')
+    b = input('segundo número: ')
+    operacao = ''
+    operacoes1 = ['soma', 'subtracao', 'divisao', 'multiplicacao']
+    operacoes2 = ['+', '-', '/', '*']
+    while operacao not in operacoes1 + operacoes2:
+        operacao = input('Qual operação? Opções válidas: soma, subtracao, divisao, multiplicacao, +, -, /, * ').strip().lower()
+    if operacao in operacoes2:
+        operacao = operacoes1[operacoes2.index(operacao)]
+    print(eval(operacao)(a, b))
+    print('fin')
+
+
