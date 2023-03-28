95631e8c12594da866f78bed7528112f98ffdfdd
LEBD <muerebillgates@hotmail.com>
2023-03-27 20:36:26 -0500

    Merge remote-tracking branch 'origin/main'

--- projetos/calculadora/calculadora/funcoes.py
+++ projetos/calculadora/calculadora/funcoes.py
@@ -1,1 +1,8 @@
 #aqui meteré las funciones
+def soma(a,b):
+    if type(a) in [int, float] and type(b) in [int, float]:
+        return a + b
+    else:
+        print(f'Os input `a` e `b` devem ser numéricos, recebido {a}, {type(a)}, {b} {type(b)}')
+
+
